92866f25176e62b16cfad9afb885f4a5e82042b2
Jiwon Park <xoxojw.dev@gmail.com>
2024-10-31 16:12:13 +0900

    chore: App.tsx와 main.tsx 구조 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import PageLayout from "@/shared/components/layout/PageLayout";
 import { Outlet } from "react-router-dom";
 
-const queryClient = new QueryClient();
-
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<PageLayout>
-				<Outlet />
-			</PageLayout>
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<PageLayout>
+			<Outlet />
+		</PageLayout>
 	);
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
@@ -9,12 +12,25 @@ import { Global } from "@emotion/react";
 import globalStyles from "@/styles/globalStyles";
 import { Toaster } from "@/shared/components/ui/toaster";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: 2,
+			staleTime: 2 * 60 * 1000, // 2분
+			gcTime: 30 * 1000, // 30초
+		},
+	},
+});
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Provider>
-			<Global styles={globalStyles} />
-			<RouterProvider router={router} />
-			<Toaster />
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<Provider>
+				<Global styles={globalStyles} />
+				<RouterProvider router={router} />
+				<Toaster />
+			</Provider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</StrictMode>
 );
