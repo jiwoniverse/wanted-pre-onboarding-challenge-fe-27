a853ec5d42efd661bb547a76c1a5ccaf3481f697
Jiwon Park <xoxojw.dev@gmail.com>
2024-10-31 10:20:32 +0900

    chore: @tanstack/react-query 라이브러리 설치

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import PageLayout from "@/shared/components/layout/PageLayout";
 import { Outlet } from "react-router-dom";
 
+const queryClient = new QueryClient();
+
 function App() {
 	return (
-		<PageLayout>
-			<Outlet />
-		</PageLayout>
+		<QueryClientProvider client={queryClient}>
+			<PageLayout>
+				<Outlet />
+			</PageLayout>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
